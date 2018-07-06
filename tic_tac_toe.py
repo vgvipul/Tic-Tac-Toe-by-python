1abd08c5d51a3d543984f895af453af681a8a445
Vipul Gupta <35928346+vgvipul@users.noreply.github.com>
2018-07-06 15:48:55 +0530

    final

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,5 +1,6 @@
 import sys
-
+import random
+ 
 l=[["-","-","-"],["-","-","-"],["-","-","-"]]
 
 
@@ -13,11 +14,11 @@ def print_board():
 
 
 def player1():
-    print("Enter index where you want to place 0")
+    print("Enter index where you want to place O")
     x=int(raw_input())
     y=int(raw_input())
     if l[x][y]=='-':
-        l[x][y]='0'
+        l[x][y]='O'
         print_board()
     else:
         print("Place already filled!!!!!! try again")
@@ -27,91 +28,113 @@ def player1():
 
 
 def player2():
-    print("Enter index where you want to place *")
+    print("Enter index where you want to place X")
     x=int(raw_input())
     y=int(raw_input())
     if l[x][y]=='-':
-        l[x][y]='*'
+        l[x][y]='X'
         print_board()
     else:
         print("Place already filled!!!!!! try again")
         player2()   
 
 def check1():
-    if l[0][0]==l[0][1]==l[0][2]=='0':
+    if l[0][0]==l[0][1]==l[0][2]=='O':
         print("-----------------player 1 WON-----------------")
         sys.exit()
         
-    elif l[1][0]==l[1][1]==l[1][2]=='0':
+    elif l[1][0]==l[1][1]==l[1][2]=='O':
         print("-----------------player 1 WON-----------------")
         sys.exit()
         
-    elif l[2][0]==l[2][1]==l[2][2]=='0':
+    elif l[2][0]==l[2][1]==l[2][2]=='O':
         print("-----------------player 1 WON-----------------")
         sys.exit()
             
-    elif l[0][0]==l[1][0]==l[2][0]=='0':
+    elif l[0][0]==l[1][0]==l[2][0]=='O':
         print("-----------------player 1 WON-----------------")
         sys.exit()
         
-    elif l[0][1]==l[1][1]==l[2][1]=='0':
+    elif l[0][1]==l[1][1]==l[2][1]=='O':
         print("-----------------player 1 WON-----------------")   
         sys.exit()
         
-    elif l[0][2]==l[1][2]==l[2][2]=='0':
+    elif l[0][2]==l[1][2]==l[2][2]=='O':
         print("-----------------player 1 WON-----------------")
         sys.exit()
         
-    elif l[0][0]==l[1][1]==l[2][2]=='0':
+    elif l[0][0]==l[1][1]==l[2][2]=='O':
         print("-----------------player 1 WON-----------------")
         sys.exit()
         
-    elif l[0][2]==l[1][1]==l[2][0]=='0':
+    elif l[0][2]==l[1][1]==l[2][0]=='O':
         print("-----------------player 1 WON-----------------")  
         sys.exit()
 
 
 def check2():
-    if l[0][0]==l[0][1]==l[0][2]=='*':
+    if l[0][0]==l[0][1]==l[0][2]=='X':
         print("-----------------player 2 WON-----------------")
         sys.exit()
         
-    elif l[1][0]==l[1][1]==l[1][2]=='*':
+    elif l[1][0]==l[1][1]==l[1][2]=='X':
         print("-----------------player 2 WON-----------------")
         sys.exit()
         
-    elif l[2][0]==l[2][1]==l[2][2]=='*':
+    elif l[2][0]==l[2][1]==l[2][2]=='X':
         print("-----------------player 2 WON-----------------")
         sys.exit()
             
-    elif l[0][0]==l[1][0]==l[2][0]=='*':
+    elif l[0][0]==l[1][0]==l[2][0]=='X':
         print("-----------------player 2 WON-----------------")
         sys.exit()
         
-    elif l[0][1]==l[1][1]==l[2][1]=='*':
+    elif l[0][1]==l[1][1]==l[2][1]=='X':
         print("-----------------player 2 WON-----------------")   
         sys.exit()
         
-    elif l[0][2]==l[1][2]==l[2][2]=='*':
+    elif l[0][2]==l[1][2]==l[2][2]=='X':
         print("-----------------player 2 WON-----------------")
         sys.exit()
         
-    elif l[0][0]==l[1][1]==l[2][2]=='*':
+    elif l[0][0]==l[1][1]==l[2][2]=='X':
         print("-----------------player 2 WON-----------------")
         sys.exit()
         
-    elif l[0][2]==l[1][1]==l[2][0]=='*':
+    elif l[0][2]==l[1][1]==l[2][0]=='X':
         print("-----------------player 2 WON-----------------") 
         sys.exit()    
-        
-print_board()
-a='y'
-while(a=='y'):
-    print("Player 1 :")
-    player1()
-    check1()
-    print("Player 2 :")
-    player2()
-    check2()
 
+
+print("WELCOME to the game of TIC TAC TOE developed by me, Vipul Gupta")
+print("read all the instructions carefully")
+print("-Index to place 0 and * are in the form of matrix i.e [0][0]")
+print("-Values for indeces varies between 0 to 2")
+print("Are you ready for thr game?(y/n)")
+v=raw_input()
+if v=='y':
+    print_board()
+    r=random.randint(1,2)
+    a='y'
+    while(a=='y'):
+        if r==1:
+            print("-------PLAYER 1 HAS WON THE TOSS-------")
+            print("Player 1 :")
+            player1()
+            check1()
+            print("Player 2 :")
+            player2()
+            check2()
+        elif r==2:
+            print("-------PLAYER 2 HAS WON THE TOSS-------")
+            print("Player 2 :")
+            player2()
+            check2()
+            print("Player 1 :")
+            player1()
+            check1()
+else:
+    sys.exit()
+
+    
 
